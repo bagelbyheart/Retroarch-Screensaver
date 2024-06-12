82aa9a630b8edae086fe45644d8195803ecfad13
Stephen Ancona <stephen@bagelbyheart.com>
2024-06-11 22:15:08 -0700

    Proof of Concept

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Retroarch-Screensaver
-Use Retroarch as a screensaver to show off your rom collection.
+
+Use Retroarch as a screensaver to show off your ROM collection.
+
+## How does it work?
+
+This tool loads Retroarch via it's command line interface, specifying a
+libretro core and a random ROM file from a provided directory, runs for five
+minutes, then resets with another random ROM file.
+
+The tool accepts three arguments:
+
+- **Retroarch Path:** The location you installed Retroarch to
+- **Core Path:** The path of the libretro core you want to use for the
+  screensaver (at this time the screensaver only supports one system at a time)
+- **ROM Directory:** The location of the ROM files you want the screensaver to
+  cycle through
---
+++ Retroarch-Screensaver.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+"""
+Use Retroarch as a screensaver to show off your ROM collection.
+"""
+
+__author__ = "Stephen Ancona"
+__version__ = "0.1.0"
+__license__ = "The Unlicense"
+
+import argparse
+import os
+import random
+import subprocess
+
+
+def path_listing(directory):
+    listing = []
+    for p, d, f in os.walk(directory):
+        for file in f:
+            full_path = os.path.join(p, file)
+            listing.append(full_path)
+    return (listing)
+
+
+def pick_random(listing):
+    random.shuffle(listing)
+    selection = listing.pop()
+    return(selection)
+
+
+def start_roms(ra_path, core_file, rom_dir, timeout):
+    roms = path_listing(rom_dir)
+    while len(roms) > 0:
+        try:
+            rom = pick_random(roms)
+            print(f'{len(roms)}: {rom}')
+            subprocess.run([ra_path, '-L', core_file, rom], timeout=timeout)
+        except:
+            pass
+    start_roms(ra_path, core_file, rom_dir, timeout)
+
+
+def main(args):
+    """ Main entry point of the app """
+    if os.path.isdir(args.retroarch_dir):
+        retroarch_dir = args.retroarch_dir
+    else:
+        print(f'{args.retroarch_dir} is not a directory.')
+        exit(1)
+    if os.path.isdir(args.rom_dir):
+        rom_dir = args.rom_dir
+    else:
+        print(f'{args.rom_dir} is not a directory.')
+        exit(1)
+    timeout = 120
+    if os.path.isfile(os.path.join(retroarch_dir, 'cores', args.core_file)):
+        core_file = os.path.join(retroarch_dir, 'cores', args.core_file)
+    else:
+        print(f'{args.core_file} is not a file.')
+        exit(1)
+    print(args)
+    ra_path = os.path.join(retroarch_dir, 'retroarch.exe')
+    start_roms(ra_path, core_file, rom_dir, timeout)
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("retroarch_dir", help="Retroarch install directory")
+    parser.add_argument("rom_dir", help="ROM file directory")
+    parser.add_argument("core_file", help="Libretro core file")
+    # Optional argument which requires a parameter (eg. -d test)
+    # parser.add_argument("-n", "--name", action="store", dest="name")
+    # Specify output of "--version"
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="%(prog)s (version {version})".format(version=__version__))
+    args = parser.parse_args()
+    try:
+        main(args)
+    except KeyboardInterrupt:
+        print("Ending!")
